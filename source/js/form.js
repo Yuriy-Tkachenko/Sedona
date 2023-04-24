94090f51dc596b7883e789e950128432a1d7e3a1
Yuriy-Tkachenko <apolo16@inbox.ru>
2023-04-24 11:10:28 +0300

    created logic for sent form

--- source/js/form.js
+++ source/js/form.js
@@ -5,6 +5,13 @@ const successPopup = popup.querySelector('.modal-popup__successfull');
 const successPopupButton = successPopup.querySelector('.modal-popup__button');
 const unsuccessPopup = popup.querySelector('.modal-popup__unsuccessfull');
 const unsuccessPopupButton = unsuccessPopup.querySelector('.modal-popup__button');
+const inputs = form.querySelectorAll('input');
+console.log(inputs);
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  successPopup.classList.remove('modal-popup__hidden');
+})
 
 successPopupButton.addEventListener('click', function () {
   successPopup.classList.add('modal-popup__hidden');
@@ -12,8 +19,4 @@ successPopupButton.addEventListener('click', function () {
 
 unsuccessPopupButton.addEventListener('click', function () {
   unsuccessPopup.classList.add('modal-popup__hidden');
-})
-
-sentButton.addEventListener('submit', function(evt) {
-  form.preventDefault();
 })
