d749a96e592f660eec119bbd7407d28ff85e3f67
Yuriy-Tkachenko <apolo16@inbox.ru>
2023-05-18 23:11:14 +0300

    created mocks data

---
+++ source/js/mocks.js
@@ -0,0 +1,95 @@
+const hotels = [
+  {
+    id: 0,
+    name: 'Amara Resort & Spa',
+    price: 4000,
+    type: 'hotel',
+    raiting: 8.9,
+    img: {
+      png1x: 'img/amara-resort@1x.png',
+      png2x: 'img/amara-resort@2x.png',
+      webp1x: 'img/amara-resort@1x.webp',
+      webp2x: 'img/amara-resort@2x.webp'
+    }
+  },
+  {
+    id: 1,
+    name: 'Desert Quail Inn',
+    price: 3000,
+    type: 'motel',
+    raiting: 8.5,
+    img: {
+      png1x: 'img/desert-quail@1x.png',
+      png2x: 'img/desert-quail@2x.png',
+      webp1x: 'img/desert-quail@1x.webp',
+      webp2x: 'img/desert-quail@2x.webp'
+    }
+  },
+  {
+    id: 2,
+    name: 'Villas at Poco Diablo',
+    price: 2000,
+    type: 'apartments',
+    raiting: 9.2,
+    img: {
+      png1x: 'img/villas@1x.png',
+      png2x: 'img/villas@2x.png',
+      webp1x: 'img/villas@1x.webp',
+      webp2x: 'img/villas@2x.webp'
+    }
+  },
+  {
+    id: 3,
+    name: 'Red rocks hotel',
+    price: 5000,
+    type: 'hotel',
+    raiting: 9.2,
+    img: {
+      png1x: 'img/red-rocks@1x.png',
+      png2x: 'img/red-rocks@2x.png',
+      webp1x: 'img/red-rocks@1x.webp',
+      webp2x: 'img/red-rocks@2x.webp'
+    }
+  },
+  {
+    id: 4,
+    name: 'Yangon',
+    price: 10000,
+    type: 'hotel',
+    raiting: 9.2,
+    img: {
+      png1x: 'img/yangon@1x.png',
+      png2x: 'img/yangon@2x.png',
+      webp1x: 'img/yangon@1x.webp',
+      webp2x: 'img/yangon@2x.webp'
+    }
+  },
+  {
+    id: 5,
+    name: 'Kayak',
+    price: 4000,
+    type: 'apartments',
+    raiting: 9.2,
+    img: {
+      png1x: 'img/kayak@1x.png',
+      png2x: 'img/kayak@2x.png',
+      webp1x: 'img/kayak@1x.webp',
+      webp2x: 'img/kayak@2x.webp'
+    }
+  },
+  {
+    id: 6,
+    name: 'Arabella',
+    price: 7000,
+    type: 'motel',
+    raiting: 9.2,
+    img: {
+      png1x: 'img/arabella@1x.png',
+      png2x: 'img/arabella@2x.png',
+      webp1x: 'img/arabella@1x.webp',
+      webp2x: 'img/arabella@2x.webp'
+    }
+  },
+]
+
+export default hotels;
